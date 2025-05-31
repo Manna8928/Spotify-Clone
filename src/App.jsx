680946d6c3a6ae0bea6c4465a68983c20cb21dbe
Manna8928 <manali.singh.1989@gmail.com>
2025-05-31 16:33:30 +0530

    player options added, current and duration time made dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, useEffect, useMemo } from 'react'
 import { SideBar } from './components/SideBar'
 import Player from './components/Player'
 import Display from './components/Display'
@@ -8,10 +8,33 @@ import { SongContext } from './components/context'
 const App = () => {
   const [track, setTrack] = useState(songsData[0]);
   const [playStatus,setPlayStatus]=useState(false);
+  const [duration,setDuration] =useState({min:0,sec:0})
+  const [currentTime,setCurrentTime] =useState({min:0,sec:0})
   const audioRef = useRef(null);
-
-  return (
-    <SongContext.Provider value={{ setTrack, audioRef, playStatus, setPlayStatus }}>
+  const seekBarRef=useRef(null);
+  async function playSong(id){
+    
+    await setTrack(songsData[id]);
+    await audioRef.current.play();
+    await setPlayStatus(true);
+    await setDuration({
+                    min : Math.floor(audioRef.current.duration / 60),
+                    sec : Math.ceil(audioRef.current.duration % 60).toString().padStart(2,'0')
+                    })  
+  
+  }
+  if (audioRef.current){
+    audioRef.current.ontimeupdate=async function(){
+  
+   setCurrentTime({
+                    min : Math.floor(audioRef.current.currentTime / 60),
+                    sec : Math.ceil(audioRef.current.currentTime % 60).toString().padStart(2,'0')
+                    })  
+ }
+}
+   
+ return (
+    <SongContext.Provider value={{ track, setTrack, audioRef, playStatus, setPlayStatus, playSong, duration,currentTime, seekBarRef }}>
       <div className='bg-[#000000] text-white h-screen'>
         <div className='h-[90%] flex'>
           <SideBar />
--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -2,48 +2,56 @@ import React, { useContext } from 'react'
 import { songsData, assets } from '../assets/assets'
 import { SongContext } from './context'
 const Player = () => {
-    const { audioRef, playstatus, setPlayStatus } = useContext(SongContext);
+    const { track, audioRef, playStatus, setPlayStatus, playSong, duration, currentTime, seekBarRef} = useContext(SongContext);
+    // console.log("current time ",audioRef.current.currentTime)
+    
     return (
         <div className='flex  h-[10%] px-4 text-white bg-black
-        items-center mt-5 justify-between'>
-            <div className='hiddedn lg:flex items-center gap-4'>
-                <img src={songsData[0].image} className='w-12 h-12'></img>
+        items-center mt-3 justify-between'>
+            <div className='hidden lg:flex items-center gap-4'>
+                <img src={track.image} className='w-12 h-12'></img>
                 <div className='flex flex-col'>
-                    <div>{songsData[0].name}</div>
-                    <div>{songsData[0].desc.substring(0, 12)}</div>
+                    <div>{track.desc.substring(0, 12)}</div>
+                    <div>{track.name}</div>
                 </div>
             </div>
             <div className='flex flex-col items-center justify-center gap-1'>
                 <div className='flex gap-2 items-center'>
                     <img src={assets.shuffle_icon} className='w-5 h-5 cursor-pointer ' />
-                    <img src={assets.prev_icon} className='w-5 h-5 cursor-pointer ' />
-                    {playstatus ?
-                        <>
-                            <img src={assets.pause_icon} className='w-5 h-5 cursor-pointer ' onClick={() => {
-                                audioRef.current.pause();
-                            }} />
-                            {setPlayStatus(false)}
-                        </>
+                    <img src={assets.prev_icon} className='w-5 h-5 cursor-pointer ' onClick={()=>{
+                        if(track.id>0)
+                            playSong(track.id-1)
+                    }} />
+                    {playStatus ?
+
+                        <img src={assets.pause_icon} className='w-5 h-5 cursor-pointer ' onClick={() => {
+                            audioRef.current.pause();
+                            setPlayStatus(false)
+                        }} />
+
                         :
-                        <>
-                            <img src={assets.play_icon} className='w-5 h-5 cursor-pointer ' onClick={() => {
-                                audioRef.current.play();
-                            }} />
-                            {setPlayStatus(true)}
-                        </>
+
+                        <img src={assets.play_icon} className='w-5 h-5 cursor-pointer ' onClick={() => {
+                            audioRef.current.play();
+                            setPlayStatus(true)
+                        }} />
+
                     }
-                    <img src={assets.next_icon} className='w-5 h-5 cursor-pointer ' />
+                    <img src={assets.next_icon} className='w-5 h-5 cursor-pointer ' onClick={()=>{
+                        if(track.id<songsData.length-1)
+                            playSong(track.id+1)
+                    }} />
                     <img src={assets.loop_icon} className='w-5 h-5 cursor-pointer ' />
                 </div>
                 <div className='flex items-center gap-2'>
-                    <div className=''>1:25</div>
+                    <div className=''>{currentTime.min}:{currentTime.sec}</div>
                     <div className='bg-gray-300 w-[60vw] max-w-[500px] cursor-pointer rounded-full'>
-                        <hr className='bg-green-800 w-0 h-1 border-none rounded-full' />
+                        <hr ref={seekBarRef} className='bg-green-800 w-0 h-1 border-none rounded-full' />
                     </div>
-                    <div className=''>3:20</div>
+                    <div className=''>{duration.min}:{duration.sec}</div>
                 </div>
             </div>
-            <div className='flex gap-2'>
+            <div className='lg:flex gap-2 hidden'>
                 <img src={assets.plays_icon} className='w-4' />
                 <img src={assets.mic_icon} className='w-4' />
                 <img src={assets.queue_icon} className='w-4' />
